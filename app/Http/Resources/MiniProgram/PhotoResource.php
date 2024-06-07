4ff96c87e4f3a3d651a32c628334dc7d8a745ccb
wertyq111 <wertyq222@163.com>
2024-06-07 09:49:50 +0800

    图片格式转换

--- app/Http/Resources/MiniProgram/PhotoResource.php
+++ app/Http/Resources/MiniProgram/PhotoResource.php
@@ -14,9 +14,14 @@ class PhotoResource extends BaseResource
      */
     public function toArray(Request $request): array
     {
+        $convertFormat = ['heic'];
+
         $data = parent::toArray($request);
 
         $data['category'] = $this->category;
+
+        $imageFormat = str_replace(".", "", strtolower(strrchr($this->url,'.')));
+        $data['url'] = in_array($imageFormat, $convertFormat) ? $data['url']. "?imageMogr2/format/jpg" : $data['url'];
 
         return $data;
     }
