26a9447236bffb2211d7fcd35f6e08325893d53d
wertyq111 <wertyq222@163.com>
2024-06-20 17:27:19 +0800

    新增区域图片删除的配置, 房屋接口同名校验新增 pid 查询字段

--- app/Services/Api/MiniProgram/ImageService.php
+++ app/Services/Api/MiniProgram/ImageService.php
@@ -16,7 +16,7 @@ class ImageService  extends BaseService
      * @author zhouxufeng <zxf@netsun.com>
      * @date 2024/6/19 16:10
      */
-    public function generateImagePath($url, $type)
+    public function generateImagePath($url, $type, $data)
     {
         $imagePath = null;
 
@@ -31,13 +31,17 @@ class ImageService  extends BaseService
                 // 获取替换值
                 foreach($paths[$type]['params'] as $param) {
                     $getParam = "get". ucfirst($param);
-                    $replaceArray[$param] = $this->$getParam();
+
+                    // 判断方法是否存在
+                    $replaceArray[$param] = method_exists($this, $getParam) ? $this->$getParam() : $data[$param];
                 }
 
                 if($imageName) {
+                    $imageDir = $paths[$type]['dir'];
                     // 对路径进行替换
                     foreach($replaceArray as $key => $value) {
-                        $imagePath = str_replace("%{$key}%", $value, $paths[$type]['dir']). $imageName;
+                        $imageDir = str_replace("%{$key}%", $value, $imageDir);
+                        $imagePath = $imageDir. $imageName;
                     }
                 }
             } else {
@@ -58,5 +62,10 @@ class ImageService  extends BaseService
     protected function getMember()
     {
         return auth('api')->user()->member->id;
+    }
+
+    protected function getParams()
+    {
+
     }
 }
